5d86442068465d031e45a84a9a6ea4b153c9dd94
Hayes II <u400265@ent.wfb.bank.corp>
2015-12-17 15:56:13 -0800

    Exceptor decorator, default map html.

--- main.py
+++ main.py
@@ -19,32 +19,42 @@ MYSECRET = "thisismyrealsecret"
 
 from models import User, public_serialize
 
-def protected(f):
+def exceptor(f):
 	@wraps(f)
-	def decorated_function(*args, **kwargs):
+	def decorated(*args, **kwargs):
 		try:
-			data = request.json
-			token = data['token']
-			tokendata = jwt.decode(token, MYSECRET, algorithms=["HS256"])
-
-			print tokendata
-			user = User.query.filter_by(username=tokendata['username']).first()
-			if user == None:
-				return jsonify(error="NO USER :O!")
-			if user.password == tokendata['password']:
-				return f(*args, **kwargs)
-			else:
-				return jsonify(error="Invalid password!")
-
+			return f(*args, **kwargs)
+		except DecodeError as e:
+			return jsonify(error=str(e) + ": Invalid token! Go login again...")
+		except ValueError as e:
+			return jsonify(error=str(e) + ": Password was stored improperly...")
 		except KeyError as e:
 			return jsonify(error=str(e) + " must be provided.")
 		except IntegrityError as e:
 			return jsonify(error=e.message)
 		except Exception as e:
-			return jsonify(error=repr(e) + "(Invalid JSON.)")
-	return decorated_function
+			return jsonify(error=repr(e) + "(Invalid JSON?)")
+	return decorated
+
+def protected(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		data = request.json
+		token = data['token']
+		tokendata = jwt.decode(token, MYSECRET, algorithms=["HS256"])
+
+		print tokendata
+		user = User.query.filter_by(username=tokendata['username']).first()
+		if user == None:
+			return jsonify(error="Invalid username! Are you tring to forge a token?")
+		if user.password == tokendata['password']:
+			return f(*args, **kwargs)
+		else:
+			return jsonify(error="Invalid password! Are you tring to forge a token?")
+	return decorated
 
 @app.route("/users", methods=["POST"])
+@exceptor
 @protected
 def users():
 	users = User.query.all()
@@ -54,49 +64,35 @@ def users():
 	return jsonify(result=results)
 
 @app.route("/signup", methods=["POST"])
+@exceptor
 def signup():
-	try:
-		data = request.json
+	data = request.json
 
-		username = data['username']
-		email = data['email']
-		password = data['password']
+	username = data['username']
+	email = data['email']
+	password = data['password']
 
-		user = User(username, email, bcrypt.generate_password_hash(password))
-		db.session.add(user)
-		db.session.commit()
-
-	except KeyError as e:
-		return jsonify(error=str(e) + " must be provided.")
-	except IntegrityError as e:
-		return jsonify(error=e.message)
-	except Exception as e:
-		return jsonify(error=repr(e) + "(Invalid JSON.)")
+	user = User(username, email, bcrypt.generate_password_hash(password))
+	db.session.add(user)
+	db.session.commit()
 
 	return jsonify(result=repr(user))
 
 @app.route("/login", methods=["POST"])
+@exceptor
 def login():
-	try:
-		data = request.json
+	data = request.json
 
-		username = data['username']
-		password = data['password']
+	username = data['username']
+	password = data['password']
 
-		user = User.query.filter_by(username=username).first()
-		if user == None:
-			return jsonify(error="Invalid username!")
-		if bcrypt.check_password_hash(user.password, password):
-			token = jwt.encode({"time": str(db.func.current_timestamp()), "username": user.username, "password": user.password}, MYSECRET, algorithm="HS256")
-			return jsonify(result="Successful login! Your authentication token is: " + token)
-		else:
-			return jsonify(error="Invalid password!")
-
-	except ValueError as e:
-		return jsonify(error=str(e) + ": Password was stored improperly...")
-	except KeyError as e:
-		return jsonify(error=str(e) + " must be provided.")
-	except IntegrityError as e:
-		return jsonify(error=e.message)
+	user = User.query.filter_by(username=username).first()
+	if user == None:
+		return jsonify(error="Invalid username!")
+	if bcrypt.check_password_hash(user.password, password):
+		token = jwt.encode({"time": str(db.func.current_timestamp()), "username": user.username, "password": user.password}, MYSECRET, algorithm="HS256")
+		return jsonify(result="Successful login! Your authentication token is: " + token)
+	else:
+		return jsonify(error="Invalid password!")
 
 app.run()
